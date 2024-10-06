65e7b419a44516168f08fe5e8e5f5cce4583573d
voytovychv <voytovychv@gmail.com>
2024-10-06 18:44:29 +0200

    added custome header filter to allow OData-Version to be passed to openui5

--- srv/src/main/java/org/gebit/config/SecurityConfig.java
+++ srv/src/main/java/org/gebit/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.gebit.config;
 
 import org.gebit.authentication.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -58,11 +62,32 @@ public class SecurityConfig {
                                         "/swagger-ui/**",
                                         "/users/register",
                                         "/services",
+                                        "/index.html",
                                         "/odata/v4/srv.registration/**"
-
-                                ).permitAll()
+                                ).  permitAll()
                                 .anyRequest().authenticated()
                 ).addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class).build();
     }
+    
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:8081"); // Allow all origins
+        config.addAllowedHeader("*");
+//        config.addAllowedOriginPattern("http://localhost**");
+        config.addAllowedMethod("*"); // Allow all methods
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 
+    
+    @Bean
+    public FilterRegistrationBean<CustomHeaderFilter> customHeaderFilter() {
+        FilterRegistrationBean<CustomHeaderFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CustomHeaderFilter());
+        registrationBean.addUrlPatterns("/*"); // Apply to all URL patterns
+        return registrationBean;
+    }
 }
