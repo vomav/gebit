a46abd89c31eac6b5ca17b95e6d2efae2dfea6e7
voytovychv <voytovychv@gmail.com>
2024-12-30 12:14:31 +0100

    Update UIResources.java

--- srv/src/main/java/org/gebit/resources/UIResources.java
+++ srv/src/main/java/org/gebit/resources/UIResources.java
@@ -1,11 +1,13 @@
 package org.gebit.resources;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("cloud")
 public class UIResources implements WebMvcConfigurer {
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
