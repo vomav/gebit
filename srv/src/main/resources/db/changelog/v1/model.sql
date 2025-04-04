1687ace2279f0f96bd9dd476ce97655fcfd602db
voytovychv <voytovychv@gmail.com>
2025-04-04 19:13:12 +0200

    add v1 version of db

--- srv/src/main/resources/db/changelog/v1/model.sql
+++ srv/src/main/resources/db/changelog/v1/model.sql
@@ -3,6 +3,7 @@ DROP VIEW IF EXISTS srv_searching_Territories;
 DROP VIEW IF EXISTS srv_admin_UserTenantMappings;
 DROP VIEW IF EXISTS srv_admin_LoggedInUser;
 DROP VIEW IF EXISTS srv_admin_Tenants;
+DROP VIEW IF EXISTS srv_searching_Images;
 DROP VIEW IF EXISTS srv_searching_InWorkBy;
 DROP VIEW IF EXISTS srv_searching_Tenants;
 DROP VIEW IF EXISTS srv_searching_TenantMappings;
@@ -10,12 +11,12 @@ DROP VIEW IF EXISTS srv_searching_PartAssignments;
 DROP VIEW IF EXISTS srv_searching_PublicTerritoryAssignments;
 DROP VIEW IF EXISTS srv_searching_TerritoryAssignments;
 DROP VIEW IF EXISTS srv_searching_Parts;
-DROP TABLE IF EXISTS db_TestSubEntity;
-DROP TABLE IF EXISTS db_TestEntity;
 DROP TABLE IF EXISTS db_Tenants;
 DROP TABLE IF EXISTS db_UserTenantMappings;
+DROP TABLE IF EXISTS db_UserAccountActivations;
 DROP TABLE IF EXISTS db_Users;
 DROP TABLE IF EXISTS db_InWorkBy;
+DROP TABLE IF EXISTS db_Image;
 DROP TABLE IF EXISTS db_PartAssignments;
 DROP TABLE IF EXISTS db_TerritoryAssignments;
 DROP TABLE IF EXISTS db_Parts;
@@ -61,6 +62,17 @@ CREATE TABLE db_PartAssignments (
   tenantDiscriminator VARCHAR(36),
   part_ID VARCHAR(36),
   isDone BOOLEAN,
+  imageUrl VARCHAR(512),
+  toParent_ID VARCHAR(36),
+  PRIMARY KEY(ID)
+); 
+
+CREATE TABLE db_Image (
+  ID VARCHAR(36) NOT NULL,
+  tenantDiscriminator VARCHAR(36),
+  imageUrl VARCHAR(512),
+  spacePath VARCHAR(512),
+  mediaType VARCHAR(64),
   toParent_ID VARCHAR(36),
   PRIMARY KEY(ID)
 ); 
@@ -68,7 +80,6 @@ CREATE TABLE db_PartAssignments (
 CREATE TABLE db_InWorkBy (
   ID VARCHAR(36) NOT NULL,
   user_ID VARCHAR(36),
-  test VARCHAR(32),
   toParent_ID VARCHAR(36),
   PRIMARY KEY(ID)
 ); 
@@ -81,6 +92,14 @@ CREATE TABLE db_Users (
   currentTenant_ID VARCHAR(36),
   password VARCHAR(128),
   refreshToken VARCHAR(2048),
+  isActivated BOOLEAN,
+  PRIMARY KEY(ID)
+); 
+
+CREATE TABLE db_UserAccountActivations (
+  ID VARCHAR(36) NOT NULL,
+  toUser_ID VARCHAR(36),
+  activationCode INTEGER,
   PRIMARY KEY(ID)
 ); 
 
@@ -98,23 +117,6 @@ CREATE TABLE db_Tenants (
   name VARCHAR(128),
   description VARCHAR(1024),
   defaultUserTenant BOOLEAN DEFAULT TRUE,
-  PRIMARY KEY(ID)
-); 
-
-CREATE TABLE db_TestEntity (
-  ID VARCHAR(36) NOT NULL,
-  testString VARCHAR(64),
-  testInt INTEGER,
-  PRIMARY KEY(ID)
-); 
-
-CREATE TABLE db_TestSubEntity (
-  ID VARCHAR(36) NOT NULL,
-  testSubString VARCHAR(64),
-  testSubInt INTEGER,
-  user_ID VARCHAR(36),
-  tenant_ID VARCHAR(36),
-  to_Entity_ID VARCHAR(36),
   PRIMARY KEY(ID)
 ); 
 
@@ -159,11 +161,14 @@ CREATE VIEW srv_searching_PartAssignments AS SELECT
   dbPartAssignmenst_0.tenantDiscriminator,
   dbPartAssignmenst_0.part_ID,
   dbPartAssignmenst_0.isDone,
+  dbPartAssignmenst_0.imageUrl,
   dbPartAssignmenst_0.toParent_ID,
   part_1.name AS name,
   part_1.coordinates AS coordinates,
-  part_1.isBoundaries AS isBoundaries
-FROM (db_PartAssignments AS dbPartAssignmenst_0 LEFT JOIN db_Parts AS part_1 ON dbPartAssignmenst_0.part_ID = part_1.ID); 
+  part_1.isBoundaries AS isBoundaries,
+  toWorkedPartImage_2.imageUrl AS workedPartImageUrl,
+  toWorkedPartImage_2.mediaType AS workedPartImageMediaType
+FROM ((db_PartAssignments AS dbPartAssignmenst_0 LEFT JOIN db_Parts AS part_1 ON dbPartAssignmenst_0.part_ID = part_1.ID) LEFT JOIN db_Image AS toWorkedPartImage_2 ON (toWorkedPartImage_2.toParent_ID = dbPartAssignmenst_0.ID)); 
 
 CREATE VIEW srv_searching_TenantMappings AS SELECT
   dbUserTenantMapping_0.user_ID,
@@ -189,9 +194,19 @@ FROM db_Tenants AS dbTenant_0;
 CREATE VIEW srv_searching_InWorkBy AS SELECT
   user_1.name AS username,
   user_1.surname AS surname,
+  user_1.ID AS userId,
   dbInworkBy_0.ID AS id,
   dbInworkBy_0.toParent_ID AS toParent_ID
 FROM (db_InWorkBy AS dbInworkBy_0 LEFT JOIN db_Users AS user_1 ON dbInworkBy_0.user_ID = user_1.ID); 
+
+CREATE VIEW srv_searching_Images AS SELECT
+  Image_0.ID,
+  Image_0.tenantDiscriminator,
+  Image_0.imageUrl,
+  Image_0.spacePath,
+  Image_0.mediaType,
+  Image_0.toParent_ID
+FROM db_Image AS Image_0; 
 
 CREATE VIEW srv_admin_Tenants AS SELECT
   dbTenant_0.ID,
@@ -210,7 +225,8 @@ CREATE VIEW srv_admin_LoggedInUser AS SELECT
   dbUser_0.name,
   dbUser_0.surname,
   dbUser_0.currentTenant_ID,
-  dbUser_0.refreshToken
+  dbUser_0.refreshToken,
+  dbUser_0.isActivated
 FROM db_Users AS dbUser_0; 
 
 CREATE VIEW srv_admin_UserTenantMappings AS SELECT
